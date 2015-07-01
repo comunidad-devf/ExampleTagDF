3dd66b3d11a01824a6210e029723392c8f0e8cd8
Salvador <salvadoredgar99@gmail.com>
2015-07-01 03:46:06 -0500

    Add Map

--- app/src/main/java/f/dev/exampletagdf/fragments/MapEventFragment.java
+++ app/src/main/java/f/dev/exampletagdf/fragments/MapEventFragment.java
@@ -9,6 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import f.dev.exampletagdf.R;
@@ -17,7 +24,7 @@ import f.dev.exampletagdf.interfaces.ReplaceToolbar;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapEventFragment extends Fragment {
+public class MapEventFragment extends Fragment implements OnMapReadyCallback {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
@@ -50,8 +57,25 @@ public class MapEventFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         replaceToolbar.onReplaceToolbar(toolbar);
+
+        initMap();
     }
 
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMyLocationEnabled(true);
+        LatLng sydney = new LatLng(19.424906, -99.194936);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title(getString(R.string.app_name)));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
+
+    }
 
     @Override
     public void onDetach() {
@@ -59,6 +83,7 @@ public class MapEventFragment extends Fragment {
 
         replaceToolbar = null;
     }
+
 
 
 }
